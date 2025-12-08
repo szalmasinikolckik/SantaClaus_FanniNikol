bcad75f0b50745778a209dc341ec96be8dc39962
szalmasi.nikol <szalmasi.nikol@ckik.hu>
2025-12-08 12:15:40 +0100

    random

--- script.js
+++ script.js
@@ -11,11 +11,26 @@ document.getElementById("generate-board").addEventListener("click", () => {
         const tr = document.createElement("tr");
 
         for (let c = 0; c < cols; c++) {
-            const td = document.createElement("td");
-            tr.appendChild(td);
+            const td = document.createElement("td");    
+            const rand = Math.random(); 
+            if (rand < 0.10) {
+                const imgA = document.createElement("img");
+                imgA.src = "images/cookie.png"; 
+                imgA.width = 40; 
+                imgA.height = 40;
+                td.appendChild(imgA);
+            } else if (rand < 0.10 + 0.05) {
+                const imgB = document.createElement("img");
+                imgB.src = "images/milk.png";
+                imgB.width = 40; 
+                imgB.height = 40;
+                td.appendChild(imgB);
+            }
+
+            tr.appendChild(td); 
         }
 
-        table.appendChild(tr);
+        table.appendChild(tr); 
     }
 
     container.appendChild(table);

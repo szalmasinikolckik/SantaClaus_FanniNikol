53c0be9a456652ff03134f418f5899387a07b2ec
fulep.fanni <fulep.fanni@ckik.hu>
2025-12-22 15:42:11 +0100

    pontok kesz! ye

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let santaCell = null; 
+let pont = 0;
+
 
 document.getElementById("generate-board").addEventListener("click", () => {
+    pont = 0;
+    document.getElementById("points-counter").textContent = "Pontok száma: 0";
+
     const rows = 10;
     const cols = 8;
 
@@ -25,7 +30,7 @@ document.getElementById("generate-board").addEventListener("click", () => {
                 imgA.width = 45;
                 imgA.height = 45;
                 td.appendChild(imgA);
-
+                
             } else if (rand < 0.15) {
                 const imgB = document.createElement("img");
                 imgB.src = "images/milk.png";
@@ -65,49 +70,62 @@ document.getElementById("generate-board").addEventListener("click", () => {
         santaCell = cell; //lementi melyik cellaban van
         santaCell.classList.add("visited");
     });
+});
 
-    document.addEventListener("keydown", (e) => {
-        if (santaCell === null) {
-            return;
-        } //ha nincs santakitty ne csinaljon semmit se
 
-        let row = santaCell.parentElement.rowIndex;
-        let col = santaCell.cellIndex;
+document.addEventListener("keydown", (e) => {
+    if (santaCell === null) {
+        return;
+    } //ha nincs santakitty ne csinaljon semmit se
 
-        if (e.key === "ArrowUp") {
-            row = row - 1;
+    let row = santaCell.parentElement.rowIndex;
+    let col = santaCell.cellIndex;
+
+    if (e.key === "ArrowUp") {
+        row = row - 1;
+    }
+
+    if (e.key === "ArrowDown") {
+        row = row + 1;
+    }
+
+    if (e.key === "ArrowLeft") {
+        col = col - 1;
+    }
+
+    if (e.key === "ArrowRight") {
+        col = col + 1;
+    }
+
+    if (e.key !== "ArrowUp" && e.key !== "ArrowDown" && e.key !== "ArrowLeft" && e.key !== "ArrowRight") 
+    {
+        return;
+    }
+
+    const table = document.getElementById("myTable");
+    if (row < 0 || row >= table.rows.length || col < 0 || col >= table.rows[0].cells.length) {
+        return;
+    }
+
+    const newCell = table.rows[row].cells[col];
+    const kep = newCell.querySelector("img");
+
+    // megeszi a sutit, tejet es adja a pontokat
+    if (kep !== null) {
+        if (kep.src.includes("milk")) {
+            pont += 2;
+        } 
+        else if (kep.src.includes("cookie")) {
+             pont += 5;
         }
 
-        if (e.key === "ArrowDown") {
-            row = row + 1;
-        }
+        kep.remove();
 
-        if (e.key === "ArrowLeft") {
-            col = col - 1;
-        }
+        document.getElementById("points-counter").textContent = "Pontok száma: " + pont;
+    }
 
-        if (e.key === "ArrowRight") {
-            col = col + 1;
-        }
-
-        if (e.key !== "ArrowUp" && e.key !== "ArrowDown" && e.key !== "ArrowLeft" &&e.key !== "ArrowRight") 
-        {
-            return;
-        }
-
-        if (row < 0 || row >= rows || col < 0 || col >= cols) {
-            return;
-        }
-
-        const newCell = table.rows[row].cells[col];
-
-        if (newCell.querySelector("img")) {
-            return;
-        }
-
-        santaCell.classList.add("visited");
-        newCell.appendChild(santaCell.querySelector("img"));
-        santaCell = newCell;
-        santaCell.classList.add("visited");
-    });
+    santaCell.classList.add("visited");
+    newCell.appendChild(santaCell.querySelector("img"));
+    santaCell = newCell;
+    santaCell.classList.add("visited");
 });
